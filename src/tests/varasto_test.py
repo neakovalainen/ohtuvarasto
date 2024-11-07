d61646d7d4c7dde0ee0c9048b66fe7bcdd383de6
neakovalainen <neakovalaine@gmail.com>
2024-11-07 16:51:22 +0200

    Week 2 exercises 3 and 4

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -33,7 +33,7 @@ class TestVarasto(unittest.TestCase):
     def test_lisaa_varastoon_liian_paljon(self):
         self.varasto.lisaa_varastoon(self.varasto.paljonko_mahtuu() + 1)
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
-        
+
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
 
